9a4082a0d32345851e24da72e48f1d1aa5060db8
BastianEngoe <92574751+BastianEngoe@users.noreply.github.com>
2024-02-14 09:28:49 +0100

    Update UIManager.cs

--- Assets/_Scripts/Systems/UIManager.cs
+++ Assets/_Scripts/Systems/UIManager.cs
@@ -14,18 +14,6 @@ public class UIManager : MonoBehaviour
     private InputAction pauseAction;
     
     [SerializeField] private TMP_Text dialogue;
-    [SerializeField] private DialogueBankScriptableObject dialogueBank;
-    private float elapsedTime;
-    private int lineIndex, eventIndex;
-    public enum Rooms
-    {
-        INTRO,
-        FARM,
-        RACE,
-        BLOCK,
-        SHOOTER
-    }
-    public Rooms curRoom;
     public GameObject pausePanel, centerUIDot;
 
     private void Awake()
@@ -33,7 +21,7 @@ public class UIManager : MonoBehaviour
         instance = this;
         playerControls = new PlayerInputActions();
     }
-
+    
     private void Start()
     {
         dialogue.text = null;
@@ -50,152 +38,21 @@ public class UIManager : MonoBehaviour
         pausePanel.GetComponent<CanvasGroup>().alpha = 0;
         pausePanel.GetComponent<CanvasGroup>().blocksRaycasts = false;
 
-        if (curRoom == Rooms.INTRO)
+        if (DialogueManager.instance.curRoom == DialogueManager.Rooms.INTRO)
         { 
             centerUIDot.GetComponent<CanvasGroup>().alpha = 1;
             Cursor.lockState = CursorLockMode.Locked;
         }
     }
 
-    private void Update()
+    public void NextSubtitle(string subtitle)
     {
-        elapsedTime += Time.deltaTime;
-
-        switch (curRoom)
-        {
-            case Rooms.INTRO: UpdateIntroRoom();
-                break;
-            
-            case Rooms.FARM: UpdateFarmRoom();
-                break;
-            
-            case Rooms.RACE: UpdateRaceRoom();
-                break;
-            
-            case Rooms.BLOCK: UpdateBlockRoom();
-                break;
-            
-            case Rooms.SHOOTER: UpdateShooterRoom();
-                break;
-        }
+        dialogue.text = subtitle;
     }
 
-    void NextLine(DialogueBankScriptableObject.DialogueLine lineType)
-    {
-        dialogue.text = lineType.dialogue;
-        if (lineType.voiceline)
-        {
-            GameManager.instance.mascotSpeaker.clip = lineType.voiceline;
-            GameManager.instance.mascotSpeaker.Play();
-            lineType.duration = lineType.voiceline.length;
-        }
-
-        if (lineType.triggerEvent)
-        {
-            Debug.Log("Event triggered from " + lineType);
-            EventManager.instance.TriggerEvent(lineType, eventIndex);
-            eventIndex++;
-        }
-
-        if (!lineType.condition)
-        {
-            InvokeRepeating("CheckForCondition", 0f, 0.25f);
-        }
-    }
-
-    private void UpdateIntroRoom()
-    {
-        if (lineIndex == dialogueBank.introLines.Count)
-        {
-            return;
-        }
-        
-        if (dialogueBank.introLines[lineIndex].duration == 0)
-        {
-            dialogueBank.introLines[lineIndex].duration = 3f;
-        }
-
-        if (elapsedTime >= dialogueBank.introLines[lineIndex].duration)
-        {
-            if (!dialogueBank.introLines[lineIndex].condition)
-            {
-                return;
-            }
-            NextLine(dialogueBank.introLines[lineIndex]);
-            lineIndex++;
-            elapsedTime = 0f;
-        }
-    }
     
-    private void UpdateFarmRoom()
-    {
-        if (lineIndex == dialogueBank.farmLines.Count)
-        {
-            return;
-        }
 
-        if (elapsedTime >= dialogueBank.farmLines[lineIndex].duration)
-        {
-            NextLine(dialogueBank.farmLines[lineIndex]);
-            lineIndex++;
-            elapsedTime = 0f;
-        }
-    }
-    
-    private void UpdateRaceRoom()
-    {
-        if (lineIndex == dialogueBank.raceLines.Count)
-        {
-            return;
-        }
-        
-        if (elapsedTime >= dialogueBank.raceLines[lineIndex].duration)
-        {
-            NextLine(dialogueBank.raceLines[lineIndex]);
-            lineIndex++;
-            elapsedTime = 0f;
-        }
-    }
-    
-    private void UpdateBlockRoom()
-    {
-        if (lineIndex == dialogueBank.blockLines.Count)
-        {
-            return;
-        }
-        
-        if (dialogueBank.blockLines[lineIndex].duration == 0)
-        {
-            dialogueBank.blockLines[lineIndex].duration = 3f;
-        }
-
-        if (elapsedTime >= dialogueBank.blockLines[lineIndex].duration)
-        {
-            NextLine(dialogueBank.blockLines[lineIndex]);
-            lineIndex++;
-            elapsedTime = 0f;
-        }
-    }
-    
-    private void UpdateShooterRoom()
-    {
-        if (lineIndex == dialogueBank.shooterLines.Count)
-        {
-            return;
-        }
-        
-        if (dialogueBank.shooterLines[lineIndex].duration == 0)
-        {
-            dialogueBank.shooterLines[lineIndex].duration = 3f;
-        }
-
-        if (elapsedTime >= dialogueBank.shooterLines[lineIndex].duration)
-        {
-            NextLine(dialogueBank.shooterLines[lineIndex]);
-            lineIndex++;
-            elapsedTime = 0f;
-        }
-    }
+   
 
     public void PauseGame() //includes many "flip-flops" 
     {
@@ -221,8 +78,6 @@ public class UIManager : MonoBehaviour
         }
             
         centerUIDot.GetComponent<CanvasGroup>().alpha = 1 - centerUIDot.GetComponent<CanvasGroup>().alpha;
-        
-        Debug.Log("Pause button pressed!");
     }
     
     private void OnEnable()
@@ -241,22 +96,5 @@ public class UIManager : MonoBehaviour
     private void PauseKey(InputAction.CallbackContext context)
     {
         PauseGame();
-    }
-
-    public void CheckForCondition()
-    {
-        Debug.Log("Checking for condition...");
-        
-        int condition;
-        GameManager.instance.RetrievePlayerInput(out condition);
-        if (condition == 1)
-        {
-            Debug.Log("Condition Nodding");
-        }
-
-        if (condition == 2)
-        {
-            Debug.Log("Condition Shaking");
-        }
     }
 }
